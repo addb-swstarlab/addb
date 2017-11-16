6a5b17f2832cdbe2cb040845d5052751a15a8d73
hanseungsung <hssung@yonsei.ac.kr>
2017-11-16 20:03:32 +0900

    [BUGFIX] Remove unnecessary tracked file from git version control

--- src/release.h
+++ src/release.h
@@ -1,3 +1,3 @@
-#define REDIS_GIT_SHA1 "076ffa17"
-#define REDIS_GIT_DIRTY "366"
-#define REDIS_BUILD_ID "localhost.localdomain-1510829931"
+#define REDIS_GIT_SHA1 "56793ff9"
+#define REDIS_GIT_DIRTY "13"
+#define REDIS_BUILD_ID "localhost.localdomain-1510830219"
