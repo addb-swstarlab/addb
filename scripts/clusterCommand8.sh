689827b98d5d3bbb73b54255c224a22f0b0d2110
Jihwan Lee <ji_hwan43@yonsei.ac.kr>
2018-10-26 15:20:08 +0900

    Modify clusterCommand script

--- scripts/clusterCommand8.sh
+++ scripts/clusterCommand8.sh
@@ -1,15 +1,18 @@
+# Get IP
+IFS=$'\n' res=(`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`)
+
 if test "$#" -ne 0; then
 	echo "[8000]"
-	./src/redis-cli -c -p 8000 -h 192.168.1.5 $@
+	./src/redis-cli -c -p 8000 -h ${res[0]} $@
 	echo "[8001]"
-	./src/redis-cli -c -p 8001 -h 192.168.1.5 $@
+	./src/redis-cli -c -p 8001 -h ${res[0]} $@
 	echo "[8002]"
-	./src/redis-cli -c -p 8002 -h 192.168.1.5 $@
+	./src/redis-cli -c -p 8002 -h ${res[0]} $@
 	echo "[8003]"
-	./src/redis-cli -c -p 8003 -h 192.168.1.5 $@
+	./src/redis-cli -c -p 8003 -h ${res[0]} $@
 	echo "[8004]"
-	./src/redis-cli -c -p 8004 -h 192.168.1.5 $@
+	./src/redis-cli -c -p 8004 -h ${res[0]} $@
 	echo "[8005]"
-	./src/redis-cli -c -p 8005 -h 192.168.1.5 $@
+	./src/redis-cli -c -p 8005 -h ${res[0]} $@
 	exit 1
 fi
