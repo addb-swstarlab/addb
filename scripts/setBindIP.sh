8292e8a03f7f0fc99f7ef3efeb2ce5f70a49b1f5
Jihwan Lee <ji_hwan43@yonsei.ac.kr>
2018-10-26 14:20:07 +0900

    Modify setBindIP script

--- scripts/setBindIP.sh
+++ scripts/setBindIP.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 ADDB_DIR=/home/addb/addb-RR
 CONF_DIR=$ADDB_DIR/conf
-if [ -z $1 ]; then
-	echo "[ERROR] Please enter bind IP in first paramter"
-	echo "Ex) ./setBindIP.sh 165.132.172.60   => Set bind 165.132.172.60"
-	exit 1;
-fi
+#if [ -z $1 ]; then
+#	echo "[ERROR] Please enter bind IP in first paramter"
+#	echo "Ex) ./setBindIP.sh 165.132.172.60   => Set bind 165.132.172.60"
+#	exit 1;
+#fi
+
+# Get IP
+IFS=$'\n' res=(`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`)
+
 for port in 8000 8001 8002 8003 8004 8005
 do
 	BIND=$(cat ${CONF_DIR}/redis_tiering_${port}.conf | grep "bind " | grep -v "#")
-	sed "s/$BIND/bind ${1}/" "${CONF_DIR}/redis_tiering_${port}.conf" >> "${CONF_DIR}/redis_tiering_${port}_new.conf"
+#	sed "s/$BIND/bind ${1}/" "${CONF_DIR}/redis_tiering_${port}.conf" >> "${CONF_DIR}/redis_tiering_${port}_new.conf"
+	sed "s/$BIND/bind ${res[0]}/" "${CONF_DIR}/redis_tiering_${port}.conf" >> "${CONF_DIR}/redis_tiering_${port}_new.conf"
 	if [ $? -eq 0 ]; then
 		mv ${CONF_DIR}/redis_tiering_${port}_new.conf ${CONF_DIR}/redis_tiering_${port}.conf
 	else
