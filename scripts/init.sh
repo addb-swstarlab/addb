fcc672a12c30f31b9cc05db4dd30159683e1349b
Jihwan Lee <ji_hwan43@yonsei.ac.kr>
2018-10-26 11:17:19 +0900

    Add init scripts for making directories

---
+++ scripts/init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p dir
+mkdir -p dir/log
+mkdir -p dir/run
+mkdir -p dir/ssd1
+mkdir -p dir/ssd2
+mkdir -p dir/ssd1/8000
+mkdir -p dir/ssd1/8001
+mkdir -p dir/ssd1/8002
+mkdir -p dir/ssd1/hadoopData
+mkdir -p dir/ssd2/8003
+mkdir -p dir/ssd2/8004
+mkdir -p dir/ssd2/8005
+mkdir -p dir/ssd2/hadoopData
