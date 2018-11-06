f83a5e8345553837e5521782a3c4a3380428221c
Jihwan Lee <ji_hwan43@yonsei.ac.kr>
2018-11-06 17:28:53 +0900

    [TRIVIAL] Delete some scripts, Modify watch script

--- pullAll.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "git pull all node from Relational_Queue branch"
-for host in cluster01 cluster05 cluster06 cluster07 cluster09 cluster13 cluster16
-do
-	ssh $host cd addb-RR; git pull origin Relational_Queue;
-	echo "$host is done."
-done
--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,1 +1,1 @@
-watch -n 0.5 "./scripts/clusterCommand8.sh dbsize"
+watch -d -n 1 "du -h dir/ssd1/8000; du -h dir/ssd1/8001; du -h dir/ssd1/8002; ./scripts/clusterCommand8.sh dbsize"
