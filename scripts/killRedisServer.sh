91c0efeb204ecc63b2d7ae201da0a2d26d3e346b
wonki choi <cwk1412@yonsei.ac.kr>
2018-11-13 18:19:05 +0900

    [FEATURE] conf remove

--- scripts/killRedisServer.sh
+++ scripts/killRedisServer.sh
@@ -2,8 +2,9 @@
 
 for I in $(ps -ef | grep redis-server | tr -s ' '| cut -d ' ' -f 2); do
   PID=$(ps -ef | grep -m1 redis-server | tr -s ' ' | cut -d ' ' -f 2)
-  PROCESS=$(ps -ef | grep -m1 redis-server | tr -s ' ' | cut -d ' ' -f 8)
-  if [ "${PROCESS}" == "./src/redis-server" ]; then
+  PROCESS1=$(ps -ef | grep -m1 redis-server | tr -s ' ' | cut -d ' ' -f 8)
+  PROCESS2=$(ps -ef | grep -m1 redis-server | tr -s ' ' | cut -d ' ' -f 9)
+  if [ "${PROCESS1}" == "./src/redis-server" ] || [ "${PROCESS2}" == "./src/redis-server" ]; then
     sudo kill -9 ${PID}
   fi
 done
