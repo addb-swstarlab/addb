1ef70bb8df1dba59e748acdcb9dc815ccc734c40
totorody <arbc139@gmail.com>
2018-11-16 18:49:46 +0900

    [TEST] Implements scan test script.

--- scanTest.sh
+++ scanTest.sh
@@ -1,41 +1,45 @@
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 1 2 3 4
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 5 6 7 8
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 9 10 11 12
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 13 14 15 16
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 17 18 19 20
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 21 22 23 24
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 25 26 27 28
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 29 30 31 32
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 1 2 3 4
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 5 6 7 8
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 9 10 11 12
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 13 14 15 16
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 17 18 19 20
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 21 22 23 24
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 25 26 27 28
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 29 30 31 32
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 1 2 3 4
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 5 6 7 8
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 9 10 11 12
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 13 14 15 16
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 17 18 19 20
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 21 22 23 24
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 25 26 27 28
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 29 30 31 32
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 1 2 3 4
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 5 6 7 8
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 9 10 11 12
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 13 14 15 16
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 17 18 19 20
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 21 22 23 24
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 25 26 27 28
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 29 30 31 32
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 1 2 3 4
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 5 6 7 8
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 9 10 11 12
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 13 14 15 16
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 17 18 19 20
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 21 22 23 24
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 25 26 27 28
-./src/redis-cli fpwrite D:{100:1:2} 1:2 4 0 29 30 31 32
-./src/redis-cli fpscan D:{100:1:2} 2,3,4
+# Use case
+# ./scanTest.sh ${TABLE_ID} ${RANDOM_PARITION} ${COLUMN_COUNT} ${SCAN_LOOK_UP_COLUMNS} ${ITERATION_COUNT}
+# Example
+# ./scanTest.sh 100 yes 4 "1,2,3"
+#   TABLE_ID=100
+#   RANDOM_PARTITION=yes
+#   COLUMN_COUNT=4
+#   SCAN_LOOK_UP_COLUMNS="1,2,3"
+#   ITERATION_COUNT=200
+
+TABLE_ID=$1
+RANDOM_PARTITION=$2
+COLUMN_COUNT=$3
+SCAN_LOOK_UP_COLUMNS=$4
+ITERATION_COUNT=$5
+
+# RANDOM_PARTITION
+if [ $RANDOM_PARTITION == 'yes' ]
+then
+  RANDOM_PARTITION_COLUMN=$(((RANDOM % COLUMN_COUNT) + 1))
+  RANDOM_PARTITION_VALUE=$(((RANDOM % 100000) + 1))
+  PARTITION="$RANDOM_PARTITION_COLUMN:$RANDOM_PARTITION_VALUE"
+else
+  PARTITION="1:2"
+fi
+
+# Varaible counter
+counter=1
+for i in $(seq 1 $ITERATION_COUNT); do
+  values=$counter
+  if [ $COLUMN_COUNT -ge 2 ]
+  then
+    for j in $(seq $(($counter + 1)) $(($counter + $COLUMN_COUNT - 1))); do
+      values="$values $j"
+    done
+  fi
+  counter=$(($counter + $COLUMN_COUNT))
+  CMD="./src/redis-cli fpwrite D:{$TABLE_ID:$PARTITION} $PARTITION $COLUMN_COUNT 0 $values"
+  echo $CMD
+  $CMD
+done
+
+CMD="./src/redis-cli fpscan D:{$TABLE_ID:$PARTITION} $SCAN_LOOK_UP_COLUMNS"
+echo $CMD
+$CMD
