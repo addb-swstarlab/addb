659739b99859ce228b7482c907de2f5d00b62758
Jihwan Lee <ji_hwan43@yonsei.ac.kr>
2018-12-07 17:04:02 +0900

    [TRIVIAL] Updates Jihwan's watch.sh script... I don't know what it is.

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #watch -d -n 1 "du -h dir/ssd1/8000; du -h dir/ssd2/8003; ./scripts/clusterCommand8.sh dbsize"
-watch -d -n 1 "du -h dir/ssd1/8000; du -h dir/ssd1/8001; du -h dir/ssd1/8002; du -h dir/ssd2/8003; du -h dir/ssd2/8004; du -h dir/ssd2/8005; ./scripts/clusterCommand8.sh dbsize"
+watch -d -n 1 "du -h dir/ssd1/8000/0:default; du -h dir/ssd1/8001/0:default; du -h dir/ssd1/8002/0:default; du -h dir/ssd2/8003/0:default; du -h dir/ssd2/8004/0:default; du -h dir/ssd2/8005/0:default; ./scripts/clusterCommand8.sh dbsize"
