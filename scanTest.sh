6950cd9410a04c41e98563d042c66d19ca3eb50e
totorody <arbc139@gmail.com>
2019-07-15 14:45:33 +0900

    [TRIVIAL] Implements 'static' test on scanTest.sh

--- scanTest.sh
+++ scanTest.sh
@@ -1,15 +1,25 @@
 # Use case
-# ./scanTest.sh TABLE_ID=${TABLE_ID} RANDOM_PARTITION=${RANDOM_PARITION} \
+# ./scanTest.sh TABLE_ID=${TABLE_ID} MODE=${MODE} \
 #               PARTITION_COUNT=${PARTITION_COUNT} \
 #               COLUMN_COUNT=${COLUMN_COUNT} \
 #               SCAN_LOOK_UP_COLUMNS=${SCAN_LOOK_UP_COLUMNS} \
 #               INSERT_ITER_CNT=${INSERT_ITER_CNT} \
 #               SCAN_ITER_CNT=${SCAN_ITER_CNT}
 
-# Example
-# ./scanTest.sh TABLE_ID=100 RANDOM_PARTITION=yes PARTITION_COUNT=100 COLUMN_COUNT=4 SCAN_LOOK_UP_COLUMNS="1,2,3" INSERT_ITER_CNT=200 SCAN_ITER_CNT=30
+# Example 1 - Randomize Test
+# ./scanTest.sh TABLE_ID=100 MODE=random PARTITION_COUNT=100 COLUMN_COUNT=4 SCAN_LOOK_UP_COLUMNS="1,2,3" INSERT_ITER_CNT=200 SCAN_ITER_CNT=30
 #   TABLE_ID=100
-#   RANDOM_PARTITION=yes
+#   MODE=random
+#   PARTITION_COUNT=100
+#   COLUMN_COUNT=4
+#   SCAN_LOOK_UP_COLUMNS="1,2,3"
+#   INSERT_ITER_CNT=200
+#   SCAN_ITER_CNT=30
+
+# Example 2 - Static Test (Always same output)
+# ./scanTest.sh TABLE_ID=100 MODE=static PARTITION_COUNT=100 COLUMN_COUNT=4 SCAN_LOOK_UP_COLUMNS="1,2,3" INSERT_ITER_CNT=200 SCAN_ITER_CNT=30
+#   TABLE_ID=100
+#   MODE=static
 #   PARTITION_COUNT=100
 #   COLUMN_COUNT=4
 #   SCAN_LOOK_UP_COLUMNS="1,2,3"
@@ -23,7 +33,7 @@ do
   VALUE=$(echo $ARGUMENT | cut -f2 -d=)
   case "$KEY" in
     TABLE_ID)             TABLE_ID=${VALUE} ;;
-    RANDOM_PARTITION)     RANDOM_PARTITION=${VALUE} ;;
+    MODE)                 MODE=${VALUE} ;;
     PARTITION_COUNT)      PARTITION_COUNT=${VALUE} ;;
     COLUMN_COUNT)         COLUMN_COUNT=${VALUE} ;;
     SCAN_LOOK_UP_COLUMNS) SCAN_LOOK_UP_COLUMNS=${VALUE} ;;
@@ -34,7 +44,7 @@ do
 done
 
 echo "TABLE_ID:             $TABLE_ID"
-echo "RANDOM_PARTITION:     $RANDOM_PARTITION"
+echo "MODE:                 $MODE"
 echo "PARTITION_COUNT:      $PARTITION_COUNT"
 echo "COLUMN_COUNT:         $COLUMN_COUNT"
 echo "SCAN_LOOK_UP_COLUMNS: $SCAN_LOOK_UP_COLUMNS"
@@ -43,17 +53,18 @@ echo "SCAN_ITER_CNT:        $SCAN_ITER_CNT"
 
 # Randomizes collecting partitions
 PARTITIONS=()
-if [ $RANDOM_PARTITION == 'yes' ]
-then
-  for i in $(seq 1 $PARTITION_COUNT); do
+for i in $(seq 1 $PARTITION_COUNT); do
+  if [ $MODE == 'random' ]; then
     RANDOM_PARTITION_COLUMN=$(((RANDOM % COLUMN_COUNT) + 1))
     RANDOM_PARTITION_VALUE=$(((RANDOM % 100000) + 1))
     PARTITION="$RANDOM_PARTITION_COLUMN:$RANDOM_PARTITION_VALUE"
     PARTITIONS+=($PARTITION)
-  done
-else
-  PARTITIONS+=("1:2")
-fi
+  else
+    # Fixes column to 1
+    PARTITION="1:$i"
+    PARTITIONS+=($PARTITION)
+  fi
+done
 
 # Varaible counter
 counter=1
@@ -66,7 +77,11 @@ for i in $(seq 1 $INSERT_ITER_CNT); do
     done
   fi
   counter=$(($counter + $COLUMN_COUNT))
-  SELECTED_PARTITION=${PARTITIONS[$RANDOM % $PARTITION_COUNT]}
+  if [ $MODE == 'random' ]; then
+    SELECTED_PARTITION=${PARTITIONS[$RANDOM % $PARTITION_COUNT]}
+  else
+    SELECTED_PARTITION=${PARTITIONS[$i % $PARTITION_COUNT]}
+  fi
   CMD="./src/redis-cli fpwrite D:{$TABLE_ID:$SELECTED_PARTITION} $PARTITION $COLUMN_COUNT 0 $values"
   echo $CMD
   $CMD
@@ -77,7 +92,11 @@ done
 # $CMD
 
 for i in $(seq 1 $SCAN_ITER_CNT); do
-  SCAN_PARTITION=${PARTITIONS[$RANDOM % $PARTITION_COUNT]}
+  if [ $MODE == 'random' ]; then
+    SCAN_PARTITION=${PARTITIONS[$RANDOM % $PARTITION_COUNT]}
+  else
+    SCAN_PARTITION=${PARTITIONS[$i % $PARTITION_COUNT]}
+  fi
   CMD="./src/redis-cli fpscan D:{$TABLE_ID:$SCAN_PARTITION} $SCAN_LOOK_UP_COLUMNS"
   echo $CMD
   $CMD
